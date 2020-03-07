abdd4adbc4f56ef3647930e2daec865b0cae0f76
Charles <cgdusek@nodehaven.com>
2020-03-07 00:36:50 -0600

    Updated Parking.sol contract

--- contracts/Parking.sol
+++ contracts/Parking.sol
@@ -18,8 +18,9 @@ contract Parking is Ownable {
 
     ERC20Token pc; // Parxos contract (pc)
     
-    constructor(address acctContAddr, address kudosContAddr) public {
+    constructor(address parxosContAddr, address spaxosContAddr) public {
         pc = ERC20Token(parxosContAddr);
+        sc = ERC721Token(spaxosContAddr);
     }
      
     struct reservationStruct {
@@ -27,12 +28,9 @@ contract Parking is Ownable {
         uint256[2] timestamps; // [beggining, ending]  All reservation and opening times are indicated by these types of pairs
     }
 
-    struct spaceStruct {
-        uint256[] openings // openings are designated by 2 consecutive numbers as a pair (array indices are kept on database for easy access and values are checked to make sure no overlapping values)
-    }
-
     mapping (uint256 => uint256[]) space; // spaceId (uint256) to openings
     mapping (uint256 => reservationStruct) reservation; // reservationId (uint256) to reservation struct
+    mapping (address => uint256[]) reservations; // customer reservations
     mapping (uint256 => address) owner; // owner of parking space
     mapping (address => uint256[]) owned; // spaces owned by user address
        
