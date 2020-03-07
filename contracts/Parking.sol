0162f30e02eb1b35578443090a9f94a9500a4ff7
Charles <cgdusek@nodehaven.com>
2020-03-07 00:31:47 -0600

    Updated Parking.sol

--- contracts/Parking.sol
+++ contracts/Parking.sol
@@ -3,6 +3,11 @@ pragma solidity ^0.6.3;
 // import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "../node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
+interface ERC721Token {
+    function safeTransferFrom(address, address, uint256) // (from, to, tokenId)
+    function approve(address, uint256) // )approved NFT controller, tokenId)
+}
+
 interface ERC20Token {
   function allowance(address, address) external returns (uint256);
   function balanceOf(address) external returns (uint256);
@@ -11,90 +16,30 @@ interface ERC20Token {
 
 contract Parking is Ownable {
 
-    AcctCont ac; // Haven Account Contract (ac)
-    ERC20Token kc; // Kudos contract (kc)
-    uint256 contKudosBal; // Kudos held by contract
-    
-    // The address of the Natural Rights Server
-    address public naturalRightsAddress;
-
-    // The address of the Account Contract
-    address public accountContractAddress;
-    
-    // The address of the Kudos Contract
-    address public kudosContractAddress;
-
-    // The address of the Document Contract
-    address public documentContractAddress;
+    ERC20Token pc; // Parxos contract (pc)
     
     constructor(address acctContAddr, address kudosContAddr) public {
-        ac = AcctCont(acctContAddr);
-        kc = ERC20Token(kudosContAddr);
+        pc = ERC20Token(parxosContAddr);
+    }
+     
+    struct reservationStruct {
+        address customer; // used address that the space is reserved to
+        uint256[2] timestamps; // [beggining, ending]  All reservation and opening times are indicated by these types of pairs
     }
 
-    // @dev Access modifier for NR-only functionality
-    modifier onlyNR() {
-        require(msg.sender == naturalRightsAddress, "not the NR Server");
-        _;
+    struct spaceStruct {
+        uint256[] openings // openings are designated by 2 consecutive numbers as a pair (array indices are kept on database for easy access and values are checked to make sure no overlapping values)
     }
 
-    // @dev Access modifier for NR-only functionality
-    modifier onlyDocuments() {
-        require(msg.sender == documentContractAddress, "not the Document Server");
-        _;
-    }
-
-    struct walletStruct {
-        uint256 kudosBalance; // Kudos balance
-    }
-
-    mapping (address => walletStruct) wallet; // natural rights Haven ID hash to public identifiers
-
-    // Only contract owner may change Natural Rights Server
-    function setNaturalRightsServer(address _newNaturalRights) public onlyOwner returns(bool success) {
-        require(_newNaturalRights != address(0), "address must not equal 0");
-        naturalRightsAddress = _newNaturalRights;
-        return true;
-    }
-
-    // Only contract owner may change Account Contract Address
-    function setAccountContract(address _newAcctContAddr) public onlyOwner returns(bool success) {
-        require(_newAcctContAddr != address(0), "address must not equal 0");
-        ac = AcctCont(_newAcctContAddr);
-        return true;
-    }
-    
-    // Only contract owner may change Kudos Contract Address
-    function setKudosContract(address _newKudosContAddr) public onlyOwner returns(bool success) {
-        require(_newKudosContAddr != address(0), "address must not equal 0");
-        kc = ERC20Token(_newKudosContAddr);
-        return true;
-    }
-
-    // Only contract owner may change Document Contract Address
-    function setDocContract(address _newDocContAddr) public onlyOwner returns(bool success) {
-        require(_newDocContAddr != address(0), "address must not equal 0");
-        documentContractAddress = _newDocContAddr;
-        return true;
-    }
-
-    // Is msg.sender authorized to sign for the account?
-    function isDevAuthorized(address _acctAddr) public returns(bool isIndeed) {
-        return ac.isDevAuthorized(_acctAddr, msg.sender);
-    }
-
-    // Check allowance from address to contract
-    function contAllowance(address _acctAddr) public returns(uint256 value) {
-        return kc.allowance(_acctAddr, address(this));
-    }
-    
-    // Add kudos from account
-    function depositKudos(address _acctAddr, uint256 value) public returns(bool success) {
-        require(isDevAuthorized(_acctAddr), "msg.sender not authorized for account");
-        require(kc.balanceOf(_acctAddr) >= value, "not enough tokens in sender's balance");
-        require(kc.allowance(_acctAddr, address(this)) >= value, "sender has not enough allowance");
-        kc.transferFrom(_acctAddr, address(this), value);
-        wallet[_acctAddr].kudosBalance += value;
-        contKudosBal += value;
+    mapping (uint256 => uint256[]) space; // spaceId (uint256) to openings
+    mapping (uint256 => reservationStruct) reservation; // reservationId (uint256) to reservation struct
+    mapping (uint256 => address) owner; // owner of parking space
+    mapping (address => uint256[]) owned; // spaces owned by user address
+       
+    // transfer Parxos
+    function transferParxos(address _fromAddr, address _toAddr) public returns(bool success) {
+        require(pc.balanceOf(_acctAddr) >= value, "not enough tokens in sender's balance");
+        require(pc.allowance(_acctAddr, address(this)) >= value, "sender has not enough allowance");
+        pc.transferFrom(_fromAddr, _toAddr, value);
         return true;
     }
